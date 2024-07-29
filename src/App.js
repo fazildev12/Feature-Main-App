0fad0626ccd09c25e5bfe46cc6fe13b47e6bb4b1
fazildev12 <fazilfazillisaf17@gmail.com>
2024-07-29 21:50:26 +0530

    added new component in window

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import LabTabs from './components/LabTabs'; 
+import { Sample } from './components/Sample';
 
 function App() {
   return (
     <div className="App">
       <LabTabs />
+      <Sample />
     </div>
   );
 }
---
+++ src/components/Sample.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const Sample = () => {
+  return (
+    <div style={{color:'red'}}>I am from extenal application</div>
+  )
+}
+
+
+// Attach to the window object
+window.MyComponent = Sample;
