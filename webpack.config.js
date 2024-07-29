6c9afb3e996f2c9ba9b1136c9e6e5097650e2f20
fazildev12 <fazilfazillisaf17@gmail.com>
2024-07-29 21:24:02 +0530

    build cdn

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path');
+
+module.exports = {
+    mode:'development',
+    entry: './src/components/LabTabs.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'my-component.bundle.js',
+        library: 'MyComponent',
+        libraryTarget: 'umd',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-react'],
+                    },
+                },
+            },
+        ],
+    },
+}
